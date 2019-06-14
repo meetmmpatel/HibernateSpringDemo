900861bb53e69c16b2820a8151d3de31a08f56ec
meetmmpatel <meetmmpatel@gmail.com>
2019-06-14 15:52:49 -0400

    First Commit

---
+++ src/test/java/com/hibernate/sprintDemo/HibernateSpringDemo/HibernateSpringDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hibernate.sprintDemo.HibernateSpringDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HibernateSpringDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
